888ee2ae5f40a85391a4320bdb62cdd805f76813
tomabechi yuri <yuri.0123h@icloud.com>
2025-11-02 10:12:27 +0900

    修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>COATHTECH</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
    @yield('css')
 </head>
 <body>
